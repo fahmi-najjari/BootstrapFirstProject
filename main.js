8f0920226c2bdc056468aaa51af8c48bd79f0ca6
fahmi-najjari <fahminajjari@outlook.com>
2024-11-13 12:48:30 +0100

    added a comment to the site using jquery

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("just checking");
+$(document).ready(function() {
+    $('#addCommentButton').on('click', function() {
+        const commentText = $('#commentInput').val();
+        
+       
+        if (commentText) {
+            
+       const newComment = $('<div class="comment"></div>').text(commentText);
+    $('#commentsSection').append(newComment);
+      $('#commentInput').val('');
+        }
+    });
+    });
