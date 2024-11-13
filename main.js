a0c9cbcf4de53162557552c4d096902f281a3419
fahmi-najjari <fahminajjari@outlook.com>
2024-11-13 13:37:05 +0100

    just finishe the comment button and clear button function

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 console.log("just checking");
 $(document).ready(function() {
-    $('#addCommentButton').on('click', function() {
+    
+    function addComment() {
         const commentText = $('#commentInput').val();
         
-       
         if (commentText) {
+          
+            const newComment = $('<div class="comment"></div>').text(commentText);
+            $('#commentsSection').append(newComment);
             
-       const newComment = $('<div class="comment"></div>').text(commentText);
-    $('#commentsSection').append(newComment);
-      $('#commentInput').val('');
+            
+            $('#commentInput').val('');
+        }
+    }
+
+    
+    $('#addCommentButton').on('click', function() {
+        addComment();
+        
+       
+        $('#commentsSection').empty();
+    });
+
+    
+    $('#commentInput').on('keypress', function(event) {
+        if (event.key === 'Enter') {
+            addComment();
         }
     });
-    });
+
+
+
+
+
+
+
+
+
+
+    
+});
